8e2d6c60fc37d476aa2398d154207f52c5899a2d
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-18 13:08:03 -0400

    Update correction form

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -11,8 +11,8 @@ consulting: true
 # Prompt the designer/user for all of this information.
 # Convert any user input to the appropriate data type.
 puts "Hi! What is your name?"
-application[:author] = gets.chomp
-puts "Good to meet you, #{application[:author]}, How old are you?"
+application[:name] = gets.chomp
+puts "Good to meet you, #{application[:name]}, How old are you?"
 application[:age] = gets.chomp.to_i
 if application[:age] == 0
   puts "Sorry, can you provide an age with digits?"
@@ -30,32 +30,34 @@ else
   application[:consulting] == true
 end
 
-#p application
-
 # Print the hash back out to the screen when the designer has answered all of the questions.
-p application
+puts application
 # Allow Corrections from user (Convert string into symbol?)
 puts "Do you want to change any of this information? (y,n)"
 updates_wanted = gets.chomp.downcase
 if updates_wanted == "y"
-  puts "Alright, would you like your listed name, age, number of children, or none? (answer with: 'name', 'age', 'children', or 'none')"
-  whichupdate = gets.chomp.downcase
-  if whichupdate == "age"
-    puts "Alright, what would you like to change your age to?"
-    application[:age] = gets.chomp.to_i
-    if application[:age] == 0
-      puts "Sorry, can you provide an age with digits?"
-      application[:age] = gets.chomp.to_i
-    end
-  elsif whichupdate == "children"
-    puts "Alright, what how many kids do you have?"
-    application[:numchildren] = gets.chomp.to_i
-  elsif whichupdate == "name"
-    puts "Ok, what is your name?"
-    application[:namespace] = gets.chomp
+  puts "Alright, would you like your listed name, age, number of children, or none? (answer with: 'name', 'age', 'numchildren', 'decortheme', 'consulting', or 'none')"
+  whichupdate = gets.chomp.downcase.to_sym
+  if whichupdate == "none"
+  else
+    application[whichupdate.to_sym] = gets.chomp
   end
+  # if whichupdate == "age"
+  #   puts "Alright, what would you like to change this to?"
+  #   application[:age] = gets.chomp.to_i
+  #   if application[:age] == 0
+  #     puts "Sorry, can you provide an age with digits?"
+  #     application[:age] = gets.chomp.to_i
+  #   end
+  # elsif whichupdate == "children"
+  #   puts "Alright, what how many kids do you have?"
+  #   application[:numchildren] = gets.chomp.to_i
+  # elsif whichupdate == "name"
+  #   puts "Ok, what is your name?"
+  #   application[:namespace] = gets.chomp
+  # end
 end
-puts "Alright, #{application[:name]}, you've filled out the application to say that you are #{application[:age]} years old, and have #{application[:numchildren]} children."
+p application
 
 # Allow Corrections from user (Convert symb to string?)
 # Print updated hash, exit
