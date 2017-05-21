60196114d07f15c96bfa0d636863fa68bd3fc4ca
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-21 15:48:44 -0400

    Create sample nested data structure in affirmentioned file

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,26 @@
+clothing_organization = {
+  'Closet' => [
+    'closet_hanger' => [
+      'long-sleeves'
+      'sweaters'
+      't-shirts'
+    ]
+    'closet_shelf' => [
+      'shorts'
+      'sleep pants'
+      'dress pants'
+    ]
+  ]
+  'Ikea Shelf' => [
+    'Jeans'
+    'drawers' = [
+      'socks'
+      'underpants'
+    ]
+  ]
+  'Small Shelf' => [
+    'shoes'
+  ]
+}
+
+p clothing_organization
