a6e08b2168aa61504e40ef86c407b5b4e165e00e
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-22 11:54:06 -0400

    Split input into separate words & downcase

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,8 +2,12 @@ def namealias (name)
   sep_name = name.split(" ")
   #create iteration/block syntax cycle for each individual name
   sep_name.each do |i|
-    puts "#{i}"
+    i.downcase!
+    #puts i
   end
   #change first with last name - split to words, reverse
   #if vowels, change to next vowels - split individual words to letters seperately, downcase, create vowels conditional, upcase first letter of each
+  return p sep_name
 end
+
+namealias("Sanjay Nelson")
