e9efe9faf8119f878bd08bc5f938a5359a09aa75
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-22 14:53:31 -0400

    Complete alias generator, begin work on Release 1

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,23 +1,46 @@
 def namealias (fullname)
-  p fullname
+  #p fullname
   names = fullname.split(" ").reverse!
   letterarray = []
   ##create iteration/block syntax cycle for each individual name
   ##change first with last name - split to words, reverse
   ##if vowels, change to next vowels - split individual words to letters seperately, downcase, create vowels conditional, upcase first letter of each
   names.map!{|sepname| sepname.downcase}
-  p names
+  #p names
   names.each { |sepname| letterarray << sepname.split('') }
-  p letterarray
-  letterarray.map do |index|
+  #p letterarray
+  vowels = ["a", "e", "i", "o", "u"]
+  specialcons  = ["z", "d", "h", "n", "t"]
+  letterarray.map! do |index|
+    #vowels = ["a", "e", "i", "o", "u"]
     index.map do |letter|
-      if ["a", "e", "i", "o", "u"].include?(letter)
-        letter.next!
+      if vowels.include?(letter)
+        vowels.rotate(1)[vowels.index(letter)]
+      elsif specialcons.include?(letter)
+        letter.next.next
+      else
+        letter.next
       end
     end
   end
-  p letterarray
-  #return p sep_name
+  #rejoin and re-capitalize names as array
+  letterarray.map! {|index| index.join.capitalize}
+  #combine each array + a space into new var to return
+  returnname = ""
+  letterarray.each do |index|
+    if letterarray.length-1 == letterarray.index(index)
+      returnname += "#{index}"
+    else
+      returnname += "#{index} "
+    end
+  end
+  return returnname
 end
 
-namealias("Sanjay Nelson")
+#namealias("Sanjay William Nelson")
+
+#Provide a user interface that lets a user enter a name and get a fake name back. Let the user do this repeatedly until they decide to quit by typing 'quit'. (They might just hit Enter to continue.)
+
+puts "Please enter name to alias. When finished, hit enter."
+input = gets.chomp
+puts "Your alias is: #{namealias(input)}"
