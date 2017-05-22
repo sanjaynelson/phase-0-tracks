a13706637ddf868342cbd1a83dc4e46d723c6813
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-22 13:35:08 -0400

    Create array + nested array from input

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,18 +1,19 @@
 def namealias (fullname)
-  names = fullname.split(" ").reverse!.downcase!
-  #p sep_name
-  #newname = []
-  #create iteration/block syntax cycle for each individual name
-  #change first with last name - split to words, reverse
+  p fullname
+  names = fullname.split(" ").reverse!
+  letters = []
+  ##create iteration/block syntax cycle for each individual name
+  ##change first with last name - split to words, reverse
   names.map! do |sepname|
-    sepname.split('')
-    p sepname
+    sepname.downcase
+  end
+  names.each { |sepname| letters << sepname.split('') }
+  letters.map!
     # names.map! do |namearry|
     #   if namearry.include?("a" || "e" || "i" || "o" || "u")
     #    namearry.next
     #  end
     # end
-  end
   # sep_name.map! do |namearry|
   #   namearry.map! do |letter|
   #     if letter.include?("a" || "e" || "i" || "o" || "u")
@@ -20,7 +21,8 @@ def namealias (fullname)
   # end
   #if vowels, change to next vowels - split individual words to letters seperately, downcase, create vowels conditional, upcase first letter of each
   #return p sep_name
-  return p names
+  p letters
+  p names
 end
 
 namealias("Sanjay Nelson")
