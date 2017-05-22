341c1d648f9e6cfdfd04fc08613835485ebda59e
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-22 11:43:13 -0400

    Pseudocode alias generator program

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,9 @@
+def namealias (name)
+  sep_name = name.split(" ")
+  #create iteration/block syntax cycle for each individual name
+  sep_name.each do |i|
+    puts "#{i}"
+  end
+  #change first with last name - split to words, reverse
+  #if vowels, change to next vowels - split individual words to letters seperately, downcase, create vowels conditional, upcase first letter of each
+end
