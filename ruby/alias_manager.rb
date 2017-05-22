20317c7679a8a45443c8eb6e2f6df5e64898e727
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-22 13:25:14 -0400

    Log unsuccessful tests

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,13 +1,26 @@
-def namealias (name)
-  sep_name = name.split(" ")
+def namealias (fullname)
+  names = fullname.split(" ").reverse!.downcase!
+  #p sep_name
+  #newname = []
   #create iteration/block syntax cycle for each individual name
-  sep_name.each do |i|
-    i.downcase!
-    #puts i
-  end
   #change first with last name - split to words, reverse
+  names.map! do |sepname|
+    sepname.split('')
+    p sepname
+    # names.map! do |namearry|
+    #   if namearry.include?("a" || "e" || "i" || "o" || "u")
+    #    namearry.next
+    #  end
+    # end
+  end
+  # sep_name.map! do |namearry|
+  #   namearry.map! do |letter|
+  #     if letter.include?("a" || "e" || "i" || "o" || "u")
+  #       letter.next
+  # end
   #if vowels, change to next vowels - split individual words to letters seperately, downcase, create vowels conditional, upcase first letter of each
-  return p sep_name
+  #return p sep_name
+  return p names
 end
 
 namealias("Sanjay Nelson")
