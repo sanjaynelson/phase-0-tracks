36fd8ceb74f49dce76a85a0619412c5126fe5533
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-24 13:44:51 -0400

    Complete reflection aspect of gps

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -102,3 +102,21 @@ food = add_item(food, "ice_cream", 4)
 food = update_quantity(food, :ice_cream, 1)
 
 print_list(food)
+
+# What did you learn about pseudocode from working on this challenge?
+# I learned about the usefulness and importance of pseudocode to plan out what specific things need to be done while coding.
+
+# What are the tradeoffs of using arrays and hashes for this challenge?
+# Arrays seemed useful for splitting strings and then working with the individual words as items, whereas hashes were useful for categorically storing these items with an associated quantity.
+
+# What does a method return?
+# The last declared/manipulated variable, or whatever 'return' tells the method to return.
+
+# What kind of things can you pass into methods as arguments?
+# Data types, variables/outputs form other methods, and more.
+
+# How can you pass information between methods?
+# Due to scope, you need to intentionally pass a variable as an input of the next variable.
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# Scope, methods, hashes, arrays. 'Return' and 'Yield' aspects of methods are still confusing, as well as a bit of the scope bit.
