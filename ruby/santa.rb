7864c6e4e7549204eea7c9b7d8640ba44c7916f5
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-25 12:37:55 -0400

    Add array of specific attributes and create new instance with each

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,6 +14,18 @@ class Santa
   end
 end
 
-santa = Santa.new
-santa.speak
-santa.eat_milk_and_cookies("chocolate")
+# santa = Santa.new
+# santa.speak
+# santa.eat_milk_and_cookies("chocolate")
+
+santas = []
+ethnicities_ary = ["black", "Latino", "Indian", "Middle-Eastern", "white"]
+genders_ary = ["female", "male", "agender", "bigender", "male"]
+ethnicities_ary.length.times do |i|
+  santas << Santa.new(genders_ary[i], ethnicities_ary[i])
+end
+
+# santas << Santa.new("male", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
