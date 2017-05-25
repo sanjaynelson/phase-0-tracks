709fdea7459bba9285685cb8d37fc8b6989c3a0d
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-24 20:20:24 -0400

    Add puppy methods for release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,25 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
+  def speak(number)
+    number.times { puts "Woof!"}
+    #number
+  end
+  def roll_over
+    puts "*rolls over*"
+  end
+  def dog_years(humanyear)
+    dogage = humanyear * 7
+    p dogage
+  end
+  def pawshake
+    puts "*lifts up paw*"
+    puts "*asks for treat*"
+  end
 end
-
+pup = Puppy.new
+pup.fetch("chewtoy")
+pup.speak(4)
+pup.roll_over
+pup.dog_years(10)
+pup.pawshake
