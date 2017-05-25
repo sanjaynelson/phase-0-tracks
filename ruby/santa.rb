a8e150153350667112f8f59c692a62f962142062
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-05-25 14:42:22 -0400

    Create get_mad_at instance method to reorder reindeer attribute

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,6 +3,7 @@ class Santa
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
     @age = 0
     puts "Initializing Santa instance ..."
   end
@@ -11,6 +12,25 @@ class Santa
   end
   def eat_milk_and_cookies(cookie)
     puts "That was a good #{cookie} cookie!"
+  end
+  def celebrate_birthday(year)
+    @age += 1
+  end
+  def get_mad_at(reindeername)
+    p @reindeer_ranking
+      @reindeer_ranking.each_with_index do |value, i|
+        if value.downcase == reindeername.downcase
+          @reindeer_ranking.delete_at(i)
+        end
+      end
+      @reindeer_ranking.push(reindeername)
+    p @reindeer_ranking
+  end
+  def age
+    @age
+  end
+  def ethnicity
+    @ethnicity
   end
 end
 
@@ -24,7 +44,9 @@ genders_ary = ["female", "male", "agender", "bigender", "male"]
 ethnicities_ary.length.times do |i|
   santas << Santa.new(genders_ary[i], ethnicities_ary[i])
 end
-
+santa1 = Santa.new("none", "none")
+santa1.get_mad_at("Dasher")
+p santa1.age
 # santas << Santa.new("male", "black")
 # santas << Santa.new("female", "Latino")
 # santas << Santa.new("bigender", "white")
