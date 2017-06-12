7f6e3d5a1fab111dba88ea9a45b1b46f03b0a27d
Sanjay Nelson <sanjaywnelson@gmail.com>
2017-06-12 00:49:55 -0400

    Completes release 0-3 by building album_tracker.rb

--- databases/album_tracker/album_tracker.rb
+++ databases/album_tracker/album_tracker.rb
@@ -4,8 +4,8 @@
 require 'sqlite3'
 
 # SQL db
-db = SQLite3::Database.new("albums.db")
-db.results_as_hash = true
+DB = SQLite3::Database.new("albums.db")
+DB.results_as_hash = true
 
 
 # table init
@@ -14,16 +14,17 @@ create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS albums(
     id INTEGER PRIMARY KEY,
     album_name VARCHAR(255),
-    artist VARCHAR(255)
-    currently_released INT
+    artist VARCHAR(255),
+    is_out INT
   )
 SQL
-db.execute(create_table_cmd)
+
+DB.execute(create_table_cmd)
 
 ## Method - SQL insert for album
 
-def album_datainsert(album_name, artist, currently_released)
-  db.execute("INSERT INTO albums (album_name, artist, currently_released) VALUES (?, ?, ?)", [album_name, artist, currently_released])
+def album_datainsert(album_name, artist, is_out)
+  DB.execute("INSERT INTO albums (album_name, artist, is_out) VALUES (?, ?, ?)", [album_name, artist, is_out])
 end
 
 ## Method - album_input
@@ -36,31 +37,51 @@ def album_input
   album_name = gets.chomp
   puts "Is #{album_name} currently released? (y/n)"
   answer = gets.chomp
-    if answer == "y"
-      currently_released = 1
+    if answer.downcase == "y"
+      cr = 1
       cr_string = "currently released"
     else
-      currently_released = 0
+      cr = 0
       cr_string = "not currently released"
     end
   puts "Storing the album #{album_name} by #{artist}, which is #{cr_string}."
-  album_datainsert(album_name, artist, currently_released)
+  album_datainsert(album_name, artist, cr)
 end
 
 
-table_query = <<-SQL
-  SELECT * FROM albums
-SQL
-
+table_query = DB.execute("SELECT * FROM albums")
 
 ## UI - Greet user, ask if user would like to list albums on list, update an album, add an album, or exit.
 
 # While a variable is untrue, loops through those options. Typing 'Exit' will provide a departing message and exit the program.
 
+# base variables
+exit = false
+greeting = <<-OPTIONS
+Welcome to the album tracker. Do you want to 'add', 'list', or 'exit'?
+OPTIONS
 
-table_query.each do |album|
-  if album[currently_released] != 0
-    puts "#{album['album_name']} by #{album['artist']} is out."
+# loop
+
+while exit == false
+  puts greeting
+  choice = gets.chomp
+  if choice.downcase == "list"
+    table_query.each do |album|
+      if album['is_out'] != 0
+        puts "#{album['album_name']} by #{album['artist']} is out."
+      else
+        puts "#{album['album_name']} by #{album['artist']} isn't out yet."
+      end
+    end
+    # puts "Want to update an item? (y/n)"
+    # update_item = gets.chomp
+  elsif choice.downcase == "add"
+    album_input
+  elsif choice.downcase == "exit"
+    puts "Thanks for using the album tracker."
+    exit = true
   else
-    puts "#{album['album_name']} by #{album['artist']} isn't out yet."
+    puts "Sorry, could not compute."
+  end
 end
